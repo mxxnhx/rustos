1792f2be99c057d9449ae7a486c4f1004159f302
mxxnhx <mhc3357@gmail.com>
2023-10-30 21:00:32 +0900

    Exit qemu when tests complete

--- src/main.rs
+++ src/main.rs
@@ -32,4 +32,22 @@ fn test_runner(tests: &[&dyn Fn()]) {
     for test in tests {
         test();
     }
+
+    exit_qemu(QemuExitCode::Success);
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
 }
